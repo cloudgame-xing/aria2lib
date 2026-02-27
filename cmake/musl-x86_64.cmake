5789df48ee09f43ea86a75af5a4b4510abeac8e3
xing <w426405@gmail.com>
2026-02-27 19:07:06 +0800

    update 10

--- cmake/musl-x86_64.cmake
+++ cmake/musl-x86_64.cmake
@@ -12,14 +12,8 @@ set(MUSL_SYSROOT "${MUSL_TOOLCHAIN}/x86_64-linux-musl")
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Force use of musl sysroot only; on x86_64 glibc host the driver may still search /usr/lib.
+# Use musl sysroot so linker does not pick host libs (consistent with arm64 toolchain file).
 set(CMAKE_SYSROOT "${MUSL_SYSROOT}")
-
-# Explicit --sysroot so compiler and linker never see host glibc (CMAKE_SYSROOT alone can be ignored).
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --sysroot=${MUSL_SYSROOT}" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --sysroot=${MUSL_SYSROOT}" CACHE STRING "" FORCE)
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--sysroot=${MUSL_SYSROOT}" CACHE STRING "" FORCE)
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--sysroot=${MUSL_SYSROOT}" CACHE STRING "" FORCE)
 
 set(CMAKE_C_COMPILER "${MUSL_TOOLCHAIN}/bin/x86_64-linux-musl-gcc")
 set(CMAKE_CXX_COMPILER "${MUSL_TOOLCHAIN}/bin/x86_64-linux-musl-g++")
